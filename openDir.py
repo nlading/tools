935975930c98887361f6333ea36bfdc4dfbfc574
Nate Lading <nlading1@gmail.com>
2019-01-11 15:05:06 -0700

    Update openDir.py
    -- New --
    Added potential for extensibility with 'commands' dictionary. 
    Added function list_stored which displays all shelved keywords and their corresponding paths.
    Left original store/recall short hands for quick use
    
    -- Bug Fixes --
    Fixed opening from clipboard

--- openDir.py
+++ openDir.py
@@ -23,13 +23,35 @@ def recall(args):
 	subprocess.Popen(r'explorer /select, ' + shelfFile[args[2]])
 
 
+def list_stored(args):
+	# Lists all keywords and paths saved
+	print("\n\nStored keywords and locations")
+	print("-"*60 + "\n")
+	keys = list(shelfFile.keys())
+	keys.sort()
+	for kw in keys:
+		print("{0:^15}| {1}".format(kw, shelfFile[kw]))
+	print("\n" + "-"*60)
+	print("\n\n")
+
+
+commands = {
+	'store': store,
+	'recall': recall,
+	'list': list_stored,
+}
+
+
 if len(sys.argv) > 1:
 	if sys.argv[1] == 'store' or sys.argv[1] == 's':
 		store(sys.argv)
 	elif sys.argv[1] == 'recall' or sys.argv[1] == 'r':
 		recall(sys.argv)
+	elif sys.argv[1] in commands:
+		cmd = commands.get(sys.argv[1])
+		cmd(sys.argv)
 else:
-	loc = pyperclip.paste()
+	subprocess.Popen(r'explorer /select, ' + pyperclip.paste())
 
 shelfFile.close()
 
